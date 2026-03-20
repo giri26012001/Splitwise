ef9cb7889053ceb9bb49216daed0e1e07610f466
giri26012001 <mbgirirajan@gmail.com>
2026-03-20 21:44:57 +0530

    Refactor tip calculator to use command line arguments

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,12 @@
+import sys
+
 print("Welcome to split calculator")
-total_bill = float(input("What was the total bill? $"))
-percentage = int(input("What percentage tip would you like to give? 0, 10, 12 or 15? "))
-split = int(input("How many people to split the bill? "))
-pay = (total_bill/split)*(1+(percentage/100))
+
+total_bill = float(sys.argv[1])
+percentage = int(sys.argv[2])
+split = int(sys.argv[3])
+
+pay = (total_bill / split) * (1 + (percentage / 100))
 pay1 = round(pay, 2)
+
 print(f"Each person should pay: ${pay1}")
